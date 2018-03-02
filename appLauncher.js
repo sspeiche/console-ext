433ce631a9677dfa61066e8b9c228b2185fbfea4
sspeiche <sspeiche@redhat.com>
2018-03-02 10:07:16 -0500

    initial commit

---
+++ appLauncher.js
@@ -0,0 +1,7 @@
+// Add items to the application launcher dropdown menu.
+window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [{
+  title: "Tectonic",                    // The text label
+  iconClass: "fa fa-dashboard",          // The icon you want to appear
+  href: "http://127.0.0.1:9000",  // Where to go when this item is clicked
+  tooltip: 'Admin UI'              // Optional tooltip to display on hover
+}];
